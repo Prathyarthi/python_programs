abb8669e358711c00be9d39fe96ed50627438f3e
Prathyarthi <prathyarti@gmail.com>
2023-11-05 22:37:10 +0530

    Added longest_word in a sentence program

---
+++ python_problems/longest_word.py
@@ -0,0 +1,7 @@
+string = (input("Enter the sentence : "))
+split_string = string.split()
+
+for i in split_string:
+    # if len(string)>=len(string[i+1]):
+        max_length = string(i)
+print(max_length)
