070f806f9eb7b0dec4fbd5427cfe88e7921a7b3b
Prathyarthi <prathyarti@gmail.com>
2023-11-10 10:46:36 +0530

    Added write() and writelines() method in files

--- python_problems/biodata_file_access.py
+++ python_problems/biodata_file_access.py
@@ -1,9 +1,21 @@
 # Reading from a file : 
-f = open('biodata.txt','r')
-data = f.read()
-# data = f.read(10)
-# data = f.readline()
-# data = f.readlines()
+# f = open('biodata.txt','r')
+# data = f.read()
+# # data = f.read(10)
+# # data = f.readline()
+# # data = f.readlines()
+# print(data)
+# print("File read successfully!")
+# f.close()
+
+
+
+
+# Writing to a file : 
+f = open('biodata_write.txt','w')
+data = f.write('hello')
+# l = ["Hello World, this is Prathyarthi"]
+# data = f.writelines(l)      # To write numerous lines,write string data into a list and pass list name into the writelines() function
 print(data)
-print("File read successfully!")
+print("File updated successfully!")
 f.close()
