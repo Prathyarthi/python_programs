7c16db6abd1b00ad45dab9b7ccaadb63922b7dbb
Prathyarthi <prathyarti@gmail.com>
2023-11-10 11:36:07 +0530

    Added file attributes

--- python_problems/biodata_file_access.py
+++ python_problems/biodata_file_access.py
@@ -12,10 +12,32 @@
 
 
 # Writing to a file : 
-f = open('biodata_write.txt','w')
-data = f.write('hello')
-# l = ["Hello World, this is Prathyarthi"]
-# data = f.writelines(l)      # To write numerous lines,write string data into a list and pass list name into the writelines() function
+# f = open('biodata_write.txt','w')
+# data = f.write('hello')
+# # l = ["Hello World, this is Prathyarthi\n",'hello']
+# # data = f.writelines(l)      # To write numerous lines,write string data into a list or tuple and pass list name into the writelines() function
+# print(data)
+# print("File updated successfully!")
+# f.close()
+
+# File Object Attributes : 
+# file.name        # returns name of the file
+# file.mode        # returns access mode of the file
+# file.closed      # Returns True if the file is open otherwise false
+# file.readable()  # Returns True if the file is readable
+# file.writable()  # Returns True if the file is writable
+# file.seekable()  # Returns True if the file is seekable
+
+
+f = open('biodata.txt','r')
+data = f.read()
 print(data)
-print("File updated successfully!")
+print(f.name)
+print(f.mode)
+print(f.closed)
+print(f.readable())
+print(f.writable())
+print(f.seekable())
+
+print("File read successfully!")
 f.close()
